06e4231243694b534305c44eaea703777db5f6a0
ianm <ian.mcintosh@masta.org>
2016-04-13 15:14:22 +0100

    Abstracted the password protection option code into a separate class to keep the main console code clean.

--- PdfManipulationTests/Program.cs
+++ PdfManipulationTests/Program.cs
@@ -41,107 +41,11 @@ namespace PdfManipulationTests
             {
                 case ("1"):
                     // run the password protect test
-                    RunPasswordProtectTest();
+                    PasswordProtectPdf.PasswordProtection.RunPasswordProtectTest();
                     break;
             }
         }
 
-        private static bool IsPasswordValid(string password)
-        {
-            return (string.IsNullOrEmpty(password)) ? false : true;
-        }
-
-        private static bool IsFilePathValid(string filePath)
-        {
-            return (string.IsNullOrEmpty(filePath) || !File.Exists(filePath) || !filePath.EndsWith(".pdf")) ? false : true;
-        }
-
-        private static void RunPasswordProtectTest()
-        {
-            Console.WriteLine("Enter a password to use for the test (password strength not important so it can be anything");
-            Console.WriteLine();
-
-            var password = Console.ReadLine();
-            Console.WriteLine();
-
-            if (!IsPasswordValid(password))
-            {
-                Console.WriteLine("You must enter SOMETHING for the password...");
-                Console.WriteLine();
-
-                password = Console.ReadLine();
-                Console.WriteLine();
-
-                if (!IsPasswordValid(password))
-                {
-                    Console.WriteLine("Ok, one more try... this time I'll quit if you don't enter a password!");
-                    Console.WriteLine();
-
-                    password = Console.ReadLine();
-                    Console.WriteLine();
-
-                    if (!IsPasswordValid(password))
-                    {
-                        Console.WriteLine("forget it!");
-                        return;
-                    }
-                }
-            }
-
-            Console.WriteLine("Now enter the location of a PDF file to encrypt (don't worry, this will create a copy and not overwrite the original)");
-            Console.WriteLine();
-
-            var filePath = Console.ReadLine();
-            Console.WriteLine();
-
-            if (!IsFilePathValid(filePath))
-            {
-                Console.WriteLine("The file path entered is not valid or the file doesn't exist, please try again...");
-                Console.WriteLine();
-
-                filePath = Console.ReadLine();
-                Console.WriteLine();
-
-                if (!IsFilePathValid(filePath))
-                {
-                    Console.WriteLine("The file path is STILL not valid, one more go or I'm taking my ball home...");
-                    Console.WriteLine();
-
-                    filePath = Console.ReadLine();
-                    Console.WriteLine();
-
-                    if (!IsFilePathValid(filePath))
-                    {
-                        Console.WriteLine("forget it, I'm off!");
-                        return;
-                    }
-                }
-            }
-
-            Console.WriteLine(string.Format("You chose: {0}; {1}", password, filePath));
-            Console.ReadKey();
-
-            Console.WriteLine("Creating a password protected copy...");
-            Console.WriteLine();
-
-            var protectedFileName = filePath.Replace(".pdf", "-protected.pdf");
-
-            byte[] userPwd = Encoding.ASCII.GetBytes(password);
-            byte[] ownerPwd = Encoding.ASCII.GetBytes(password);
-
-            PdfReader reader = new PdfReader(filePath);
-
-            PdfStamper stamper = new PdfStamper(reader, new FileStream(protectedFileName, FileMode.Create));
-
-            stamper.SetEncryption(userPwd, ownerPwd, PdfWriter.AllowPrinting, PdfWriter.ENCRYPTION_AES_128);
-            stamper.Close();
-
-            Console.WriteLine(string.Format("All done, you should now have a protected file called {0}", protectedFileName));
-            Console.WriteLine();
-
-            Console.WriteLine("Press any key to exit");
-
-            Console.ReadKey();
-        }        
+              
     }
 }
